eedaa05fafb34c9dc858d7cfd4742930b0d355fd
Carlos eduardo <duducp2013@gmail.com>
2017-04-14 16:11:55 -0300

    Removido páginas desnecessárias

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,6 @@ import { Nav, Platform } from 'ionic-angular';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
-import { HomePage } from '../pages/home/home';
-import { ListPage } from '../pages/list/list';
 import { LoginPage } from '../pages/login/login';
 
 @Component({
@@ -22,8 +20,7 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      {title: 'Login', component: LoginPage}
     ];
 
   }
