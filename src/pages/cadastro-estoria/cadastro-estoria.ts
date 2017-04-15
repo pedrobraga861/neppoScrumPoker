30a3b6aa3cb19f333d277f0c6ef1d2ec23335a10
Carlos Eduardo <dudu.oo@outlook.com>
2017-04-15 19:45:49 -0300

    Adicionado validação dos campos requeridos e desativado o side menu

--- src/pages/cadastro-estoria/cadastro-estoria.ts
+++ src/pages/cadastro-estoria/cadastro-estoria.ts
@@ -1,13 +1,25 @@
 import {Component} from '@angular/core';
-import {NavController, NavParams} from 'ionic-angular';
+import {NavController, NavParams, MenuController} from 'ionic-angular';
+import {FormBuilder, Validators, FormGroup} from "@angular/forms";
 
 @Component({
   selector: 'page-cadastro-estoria',
   templateUrl: 'cadastro-estoria.html',
 })
 export class CadastroEstoriaPage {
+  private form: FormGroup;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public fb: FormBuilder, public menuCtrl: MenuController) {
+    menuCtrl.enable(false);
+
+    this.form = fb.group({
+      'titulo': [null, Validators.required],
+      'descricao': [null, Validators.required]
+    })
+  }
+
+  submit() {
+
   }
 
 }
